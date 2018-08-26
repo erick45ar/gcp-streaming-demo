fa6c392e76ee1ec8b1c7f500a2f6ff5353ab57c7
erick45ar <38710714+erick45ar@users.noreply.github.com>
2018-08-26 15:28:55 -0700

    Update ingest_chicago.py

--- chicago_traffic/ingest_chicago.py
+++ chicago_traffic/ingest_chicago.py
@@ -18,23 +18,23 @@ import os
 import shutil
 import logging
 import os.path
-#import zipfile
-#import datetime
+import zipfile
+import datetime
 import tempfile
 from urllib2 import urlopen
 from google.cloud import storage
 from google.cloud.storage import Blob
 
 
-def download (ChicagoData, Historical, destdir):
+def download (name1, name2, destdir):
    '''
      Downloads on-time performance data and returns local filename
    '''
-   logging.info('Requesting data for {}-{}-*'.format(ChicagoData, Historical))
+   logging.info('Requesting data for {}-{}-*'.format(name1, name2))
    
    url='https://data.cityofchicago.org/api/views/n4j6-wkkf/rows.csv?accessType=DOWNLOAD'
    
-   filename = os.path.join(destdir, "{}{}.csv".format(ChicagoData, Historical))
+   filename = os.path.join(destdir, "{}{}.csv".format(name1, name2))
    with open(filename, "wb") as fp:
      response = urlopen(url)
      fp.write(response.read())
@@ -49,21 +49,6 @@ class UnexpectedFormat(Exception):
    def __init__(self, message):
       self.message = message
  
-def verify_ingest(filename):
-   expected_header = 'SEGMENTID,STREET,DIRECTION,FROM_STREET,TO_STREET,LENGTH,STREET_HEADING,COMMENTS,START_LONGITUDE,START_LATITUDE,END_LONGITUDE,END_LATITUDE,CURRENT_SPEED,LAST_UPDATED'
-   with open(filename, 'r') as csvfp:
-      firstline = csvfp.readline().strip()
-      if (firstline != expected_header):
-         os.remove(filename)
-         msg = 'Got header={}, but expected={}'.format(firstline, expected_header)
-         logging.error(msg)
-         raise UnexpectedFormat(msg)
-
-      if next(csvfp, None) == None:
-         os.remove(filename)
-         msg = ('Received a file from Chicago that has only the header and no content')
-         raise DataUnavailable(msg)
-
 def upload(filename, bucketname, blobname):
    client = storage.Client()
    bucket = client.get_bucket(bucketname)
@@ -73,7 +58,6 @@ def upload(filename, bucketname, blobname):
    logging.info('Uploaded {} ...'.format(gcslocation))
    return gcslocation
 
- 
 def ingest(bucket):
    '''
    ingest chicago data from chicago traffic website to Google Cloud Storage
@@ -82,22 +66,21 @@ def ingest(bucket):
    '''
    tempdir = tempfile.mkdtemp(prefix='ingest_chicago')
    try:
-      filename = download(tempdir)
-      verify_ingest(filename)
+      filename = download(name1, name2, tempdir)
       gcsloc = 'chicagodata/raw/{}'.format(os.path.basename(filename))
       return upload(filename, bucket, gcsloc)
    finally:
       logging.debug('Cleaning up by removing {}'.format(tempdir))
       shutil.rmtree(tempdir)
-      
+
+            
 import argparse
-   parser = argparse.ArgumentParser(description='ingest flights data from BTS website to Google Cloud Storage')
+   parser = argparse.ArgumentParser(description='ingest traffic data from Chicago website to Google Cloud Storage')
    parser.add_argument('--bucket', help='GCS bucket to upload data to', required=True)
   
    try:
       logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
-      args = parser.parse_args()
-      
+      args = parser.parse_args() 
       logging.debug('Ingesting bucket={}'.format(bucket))
       gcsfile = ingest(args.bucket)
       logging.info('Success ... ingested to {}'.format(gcsfile))
