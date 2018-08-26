5df4c85df4c0a44fb9d8de4668fe7d1d3336768d
erick45ar <38710714+erick45ar@users.noreply.github.com>
2018-08-25 23:43:57 -0700

    Delete download.sh

--- 02_ingest/download.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-RESPONSE=$(:~$ curl https://data.cityofchicago.org/api/views/n4j6-wkkf/rows.csv?accessType=DOWNLOAD --output Chicago_Traffic_Tracker_-_Congestion_Estimates_by_Segments.csv)
-
-echo "Received $RESPONSE"
-
-done
